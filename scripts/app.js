779d59d392984b9eae07de889333436afb07512c
Cole Chambers <cole.chambers@icloud.com>
2016-10-18 16:27:31 -0600

    added controller

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+.controller("mainCtrl", function($scope) {
+  $scope.helloworld = function() {
+    console.log("Hello There!  This is the helloWorld controller function, in the mainCtrl");
+  };
+});
